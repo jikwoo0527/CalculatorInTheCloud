a6fb479fda318310bafdacae7d5139aa02d8877a
jikwoo0527 <162891330+jikwoo0527@users.noreply.github.com>
2025-11-10 11:45:54 +0000

    [Add] CalClient.java

---
+++ CalClient.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.net.*;
+
+
+public class CalClient {
+    public static void main(String argv[]) throws Exception {
+        String sentence;
+        String answer;
+        String serverIP = "";
+        int nPort = 0;
+
+        String status;
+
+        // serverIP, nPort
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/server_info.dat"))) {
+            serverIP = reader.readLine();
+            String temp = reader.readLine();
+            nPort = Integer.parseInt(temp);
+        } finally {
+            serverIP = "127.0.0.1";
+            nPort = 6789;
+        }
+
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        Socket clientSocket = new Socket(serverIP, nPort); // TCP connection
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        System.out.print("Enter the Sentence: ");
+        sentence = inFromUser.readLine();
+        outToServer.writeBytes(sentence + '\n');
+
+        status = inFromServer.readLine();
+        answer = inFromServer.readLine();
+        System.out.println("Status: " + status);
+        System.out.println(answer);
+        inFromServer.close();
+        outToServer.close();
+        clientSocket.close();
+    }
+}
